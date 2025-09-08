185b1c8c8c2f9074825d10a96415fd55ac657a98
amitpadhan525 <amitpadhan525@gmail.com>
2025-09-08 18:06:26 +0530

    Updated project

--- auto_push.sh
+++ auto_push.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-cd /home/amit/Desktop/my_vs_code_files || exit
-git add .
-git commit -m "Auto update on $(date '+%Y-%m-%d %H:%M:%S')"
-git push origin master
+sdbfjkbsdjknfknsdf
+sdbfjkbsdjknfknsdff
+sdbfjkbsdjknfknsdff
+sdbfjkbsdjknfknsdff
+sdbfjkbsdjknfknsdff
+sd
+my_vs_code_filess
+df
