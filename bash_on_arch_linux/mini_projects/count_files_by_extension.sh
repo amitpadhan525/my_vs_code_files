f85a056c0c5dad1356d9cad5ce90e716a8a452f5
amitpadhan525 <amitpadhan525@gmail.com>
2025-10-10 19:07:35 +0530

    initial commit - 2025-10-10 19:07:35

---
+++ bash_on_arch_linux/mini_projects/count_files_by_extension.sh
@@ -0,0 +1,8 @@
+path=/home/spider/my_vs_code_files
+
+for file in "$path"/*; do
+    if [ -f "$file" ] && [[ "$file" == *.* ]]; then
+        ext="${file##*.}"
+        echo "$ext"
+    fi
+done | sort | uniq -c | sort -nr
