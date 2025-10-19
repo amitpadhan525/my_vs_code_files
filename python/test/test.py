2a57aa6e7e19513dae4db48deda213883c9ed12d
amitpadhan525 <amitpadhan525@gmail.com>
2025-10-19 21:00:59 +0530

    edited - 2025-10-19 21:00:59

--- python/test/test.py
+++ python/test/test.py
@@ -1,41 +1,20 @@
-# def strip(n):
-#     if n%2==1:
-#         print('Weird')
-#     elif n%2==0&2<=n>=5:
-#         print("Not Weird")
-#     elif(n%2==0&6<=n>=20):
-#         print("Weird")
-#     elif n%2==0&n<20:
-#         print("Not Weird")
-# strip(3)
+# a=int(input("Enter 1st value"))
+# b=int(input("Enter 2nd value"))
+# # if a-b<10 or a+b<10 or (a in (5,50)):
+# #     print("True")
+# # else:
+# #     print("False")
+# print(a-b<10 or a+b<10 or (a in (5,50)))
+      
 
 
-# if __name__ == '__main__':
-#     n = int(input())
-#     arr = map(int, input().split())
+# a=int(input("Enter age"))
+# s=input("status")
+
+# print(a in (12,60) or s=="yes")
+
+a=int(input("enter 1st "))
+b=int(input("enter 2nd "))
 
 
-
-# import os
-# import random
-# for i in range(100):
-#     number=int(input("choose a number between 1 to 5 :"))
-#     if number==random.randint(1,5):
-#         print("you win")
-#     else:
-#         os.remove("/home/amit/Desktop/amit.txt")
-#         print("you lost file")
-
-# arr = [1, 2, 3]
-# print('     '.join(str(i) for i in arr))
-
-
-
-
-aa=input("enter name of file :")
-bb=input("enter what to write :")
-with open(aa,"w") as file:
-    file.write(bb)
-
-
-    
+print(a>15 or b>15)
