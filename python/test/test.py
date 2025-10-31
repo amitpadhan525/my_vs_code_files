acd2630327c094455467edb5b8d38d8df1ed6118
amitpadhan525 <amitpadhan525@gmail.com>
2025-10-31 16:21:50 +0530

    uplode - 2025-10-31 16:21:50

--- python/test/test.py
+++ python/test/test.py
@@ -1,34 +1,9 @@
 
-# Define the heart patterns for each color
-colors = ["❤", "💚", "💜", "💖", "💙"]
-lines = [
-    "Ananya I{}You{}",
-    "Ananya I{}You{}",
-    "Ananya I{}You{}",
-    "Ananya I{}You{}",
-    "Ananya I{}You{}",
-    "Ananya I{}You{}",
-    "Ananya I{}You{}",
-    "Ananya I{}You{}",
-    "Ananya I{}You{}",
-    "Ananya I{}You{}",
-    "Ananya I{}You{}",
-    "Ananya I{}You{}",
-       ]
-def print_pattern(color):
-    # Top part
-    for i in range(3):
-        print(f"{color}happy{color}diwali{color}")
-    # Pyramid top
-    for i in range(1, 6):
-        print(f"{'-'*i}{color}happy{color}diwali{color}")
-    # Pyramid bottom
-    for i in range(5, 0, -1):
-        print(f"{'-'*i}{color}happy{color}diwali{color}")
-    # Bottom part
-    for i in range(3):
-        print(f"{color}happy{color}diwali{color}")
 
-# Print the pattern for each color
-for color in colors:
-    print_pattern(color)
+a=int(input())
+b=int(input())
+c=int(input())
+if a-b<25 and b-c<25 and c-a<25:
+    print("Yes")
+else:
+    print("No")
